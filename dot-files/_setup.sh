6c636c541cfa0c16123364d80b61c9b0b9d0018b
René Nyffenegger <mail@renenyffenegger.ch>
2017-12-03 09:17:55 +0100

    + _setup.sh

---
+++ dot-files/_setup.sh
@@ -0,0 +1,6 @@
+ln -s bash_profile    ~/.bash_profile
+ln -s bashrc          ~/.bashrc
+ln -s inputrc         ~/.inputrc
+ln -s profile         ~/.profile
+ln -s xinitrc         ~/.xinitrc
+ln -s ls_colors       ~/.ls_color
