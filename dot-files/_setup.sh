e8fbc2e939a87a1abee3386128afda7de3d88ec0
René Nyffenegger <rene.nyffenegger@adp-gmbh.c>
2018-02-05 21:25:56 +0100

    dot-files/_setup.sh: mklink()

--- dot-files/_setup.sh
+++ dot-files/_setup.sh
@@ -1,7 +1,1 @@
-ln -s $(pwd)/tq84-dotfile-sequence    ~/.tq84-dotfile-sequence
-ln -s $(pwd)/bash_profile             ~/.bash_profile
-ln -s $(pwd)/bashrc                   ~/.bashrc
-ln -s $(pwd)/inputrc                  ~/.inputrc
-ln -s $(pwd)/profile                  ~/.profile
-ln -s $(pwd)/xinitrc                  ~/.xinitrc
-ln -s $(pwd)/ls_colors                ~/.ls_colors
+mklink() {  local file=$1  if [[ -f ~/.$file ]]; then    echo "~/.$file exists, deleting."    rm ~/.$file  fi  ln -s $(pwd)/$file ~/.$file}mklink tq84-dotfile-sequencemklink bash_profilemklink bashrcmklink inputrcmklink profilemklink xinitrcmklink ls_colors# ln -s $(pwd)/tq84-dotfile-sequence    ~/.tq84-dotfile-sequence# ln -s $(pwd)/bash_profile             ~/.bash_profile# ln -s $(pwd)/bashrc                   ~/.bashrc# ln -s $(pwd)/inputrc                  ~/.inputrc# ln -s $(pwd)/profile                  ~/.profile# ln -s $(pwd)/xinitrc                  ~/.xinitrc# ln -s $(pwd)/ls_colors                ~/.ls_colors
