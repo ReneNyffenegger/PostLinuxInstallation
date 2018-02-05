1c7f3ad3a893e3b1ee5cfc964a25706a5a918e82
René Nyffenegger <rene.nyffenegger@adp-gmbh.c>
2018-02-05 21:48:51 +0100

    trying to install gvim with ruby

--- install/vim/ubuntu.sh
+++ install/vim/ubuntu.sh
@@ -2,4 +2,10 @@
 # sudo apt -y install vim
 
 # Also install gvim
-sudo apt -y install vim-gnome
+# sudo apt -y install vim-gnome
+
+# gvim with ruby
+# sudo apt remove vim-gnome
+#
+# Well, the following still does not seem to install ruby with gvim
+sudo apt -y install ruby rubygems vim-nox
