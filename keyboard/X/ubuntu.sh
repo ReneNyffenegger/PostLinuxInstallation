80b610f60da3ce5e32773dfba2122afa37755df2
René Nyffenegger <rene.nyffenegger@adp-gmbh.c>
2018-02-06 20:45:30 +0100

    Swap right windows with right control

--- keyboard/X/ubuntu.sh
+++ keyboard/X/ubuntu.sh
@@ -1,4 +1,11 @@
+#
+# caps:swapescape     - Swap ESC and Caps Lock
+# ctrl:swap_rwin_rctl - Swap Right Win key with Right Ctrl key
+#
+# See /usr/share/X11/xkb/rules/evdev.lst for more options
+#
+dconf write /org/gnome/desktop/input-sources/xkb-options "['caps:swapescape', 'ctrl:swap_rwin_rctl']"
+
+# Old…
 # xkbdir=/usr/share/X11/xkb/
 # sudo cp ${xkbdir}symbols/us ${xkbdir}symbols/tq84
-
-dconf write /org/gnome/desktop/input-sources/xkb-options "['caps:swapescape']"
