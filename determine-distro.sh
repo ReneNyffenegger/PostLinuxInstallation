5576196b49f856aeaa43d22fdd6bc8adec5760d0
René Nyffenegger <rene.nyffenegger@adp-gmbh.c>
2018-02-13 22:35:17 +0100

    + determine-distro.sh

---
+++ determine-distro.sh
@@ -0,0 +1,9 @@
+if [ -f /etc/lsb-release -o -d /etc/lsb-release.d ]; then
+    echo $(lsb_release -i | cut -d: -f2 | sed s/'^\t'//)
+elif [ -f /etc/os-release ]; then
+    echo $( . /etc/os-release; echo $NAME ) 
+else
+   echo "???"
+    # ??? echo $(ls -d /etc/[A-Za-z]*[_-][rv]e[lr]* | grep -v "lsb" | cut -d'-' -f1 | cut -d'_' -f1)
+fi 
+
