5d4f3f047b609cecd07fdfa79b6d842c6249c7f8
René Nyffenegger <rene.nyffenegger@adp-gmbh.c>
2018-02-14 07:38:23 +0100

    + Arch/apps.sh

--- Arch.sh
+++ Arch.sh
@@ -2,14 +2,3 @@
 #   To do same thing temporarily: sudo rmmod pcspkr
 sudo sh -c 'echo "blacklist pcspkr" > /etc/modprobe.d/nobeeb.conf'
 
-sudo pacman -Sy xorg-server xorg-xinit
-sudo pacman -Sy xf86-video-intel # Video driver. Determine correct one with 
-                                 #    lspci | grep VGA # and
-                                 #    pacman -Ss  xf86-video
-                                 # Apparently >> xf86-video-vesa << works almost everywhere
-                                 #
-sudo pacman -Sy mesa # mesa provides the DRI driver for 3D acceleration
-sudo pacman -Sy openbox
-sudo pacman -Sy tint2
-sudo pacman -Sy ttf-dejavu
-sudo pacman -Sy xterm
