1dc9f5df749e4c5ef14521482c9efb098d5b3049
René Nyffenegger <rene.nyffenegger@adp-gmbh.c>
2018-02-14 08:22:07 +0100

    Added LWP::Protocoll::https

---
+++ install/perl/modules.sh
@@ -0,0 +1,38 @@
+#
+#  Apparently, cpan should not be run on arch!
+#
+
+distro=$(../../determine-distro.sh)
+
+if command -v make > /dev/null; then
+   echo make is installed
+else
+   echo make is not installed
+   sudo apt install make
+fi
+
+install_module() {
+  local module_name=$1
+  local arch_name=$2
+
+  if [[ $distro == 'Arch Linux' ]]; then
+
+    if [[ $arch_name == 'todo' ]]; then
+      echo "todo $module_name"
+    else
+      sudo pacman -Sy $arch_name
+    fi
+
+  else
+     echo sudo cpan $module_name
+  fi
+     
+}
+
+install_module Digest::MD5::File         todo
+install_module File::Which               todo
+install_module File::HomeDir             todo
+install_module File::Touch               todo
+install_module File::DirList             todo
+install_module Net::FTP::File            todo
+install_module LWP::Protocoll::https     perl-lwp-protocol-https  # TODO: apparently already installed on Ubuntu...
