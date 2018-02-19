b05794474e17c3e9cf1336623a4d2e62be1993bc
René Nyffenegger <rene.nyffenegger@adp-gmbh.c>
2018-02-19 22:11:10 +0100

    ntfs-3g

--- Arch.sh
+++ Arch.sh
@@ -3,5 +3,5 @@
 sudo sh -c 'echo "blacklist pcspkr" > /etc/modprobe.d/nobeeb.conf'
 
 sudo pacman -Sy zip unzip
-
 sudo pacman -Sy xdg-utils
+sudo pacman -Sy ntfs-3g    # https://unix.stackexchange.com/a/195848/6479
