16ed4fa98c812cd372aa3deb5360dde9e86a4f93
René Nyffenegger <mail@renenyffenegger.ch>
2018-03-10 20:10:52 +0100

    Defaults umask*

--- install/sudo/sodoer.sh
+++ install/sudo/sodoer.sh
@@ -4,5 +4,11 @@ su -c "cat >> /etc/sudoers <<EOF
 # Don't ask for a password when executing sudo:
 #
 rene ALL=(ALL) NOPASSWD:ALL
+
+#
+# Execute commands with umask=0022
+#
+Defaults umask_override
+Defaults umask=0022
 EOF
 "
