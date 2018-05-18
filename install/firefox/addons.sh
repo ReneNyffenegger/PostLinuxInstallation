47f64e7758223a535a9a43eaadc96d837d4d7d21
René Nyffenegger <mail@renenyffenegger.ch>
2018-05-18 21:45:47 +0200

    + vimium FF

--- install/firefox/addons.sh
+++ install/firefox/addons.sh
@@ -2,7 +2,10 @@
 # https://askubuntu.com/questions/73474/how-to-install-firefox-addon-from-command-line-in-scripts
 # 
 
-sudo wget https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/mozilla/mozilla-extension-manager
+# 2018-05-18: rm sudo.
+# sudo wget https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/mozilla/mozilla-extension-manager
+       wget https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/mozilla/mozilla-extension-manager
+
 chmod +x mozilla-extension-manager
 
 
@@ -19,3 +22,11 @@ chmod +x mozilla-extension-manager
 # AdBlockPlus
 # ~/github/github/ubuntu-scripts/mozilla/mozilla-extension-manager --user --install https://addons.mozilla.org/firefox/downloads/latest/1865/addon-1865-latest.xpi
   ./mozilla-extension-manager                                      --user --install https://addons.mozilla.org/firefox/downloads/latest/1865/addon-1865-latest.xpi
+
+# vimium FF
+#
+#     2018-05-18: Unfortunately, that seems not to work.
+#                 Somehow, it is installed, but not recognized.
+#                 Is it because it needs to access bookmarks etc?
+#
+  ./mozilla-extension-manager                                      --user --install https://addons.mozilla.org/firefox/downloads/latest/808538/addon-808538-latest.xpi
